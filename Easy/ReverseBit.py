a1da7cb723ac8ab8d5df78dc0aaa5495944cf2fa
Joel Richards <47572505+jacr20@users.noreply.github.com>
2020-03-03 00:53:09 +0000

    Delete old file

--- Easy/ReverseBit.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def reverse(x):
-        int('{0:b}'.format(x)[::-1],2)
- 
-if __name__ == '__main__':
-    print(Solution.reverse(x = 123))
-    print(int('{0:b}'.format(123)[::-1], 2))
